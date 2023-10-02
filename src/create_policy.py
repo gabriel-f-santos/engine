4431247b6b59b17038a5d26d1c05f4248ed313fd
gabriel <gabrielfgsantos95@gmail.com>
2023-10-02 00:45:29 -0300

    test

--- src/create_policy.py
+++ src/create_policy.py
@@ -5,41 +5,45 @@ import db_session
 
 
 def lambda_handler(event, context):
-    body = json.loads(event["body"])
-    tenant_id = context["authorizer"].get("tenant_id")
-    with db_session.create_session() as session:
-        tenant = session.query(Tenant).filter_by(id=tenant_id).first()
+    # body = json.loads(event["body"])
+    # tenant_id = context["authorizer"].get("tenant_id")
+    # with db_session.create_session() as session:
+    #     tenant = session.query(Tenant).filter_by(id=tenant_id).first()
 
-    if tenant is None:
-        return {
-            "statusCode": HTTPStatus.FORBIDDEN.value,
-            "body": json.dumps({"message": "forbidden"}),
-        }
+    # if tenant is None:
+    #     return {
+    #         "statusCode": HTTPStatus.FORBIDDEN.value,
+    #         "body": json.dumps({"message": "forbidden"}),
+    #     }
 
-    if not tenant.is_active:
-        return {
-            "statusCode": HTTPStatus.UNAUTHORIZED.value,
-            "body": json.dumps({"message": "forbidden"}),
-        }
-    with db_session.create_session() as session:
-        existing_policy = (
-            session.query(Policy).filter_by(tenant_id=tenant_id).first()
-        )
+    # if not tenant.is_active:
+    #     return {
+    #         "statusCode": HTTPStatus.UNAUTHORIZED.value,
+    #         "body": json.dumps({"message": "forbidden"}),
+    #     }
+    # with db_session.create_session() as session:
+    #     existing_policy = (
+    #         session.query(Policy).filter_by(tenant_id=tenant_id).first()
+    #     )
 
-        if existing_policy:
-            existing_policy.name = body["name"]
-            existing_policy.policy_details = body["policy_details"]
-            session.merge(existing_policy)
-        else:
-            policy = Policy(
-                name=body["name"],
-                tenant_id=tenant_id,
-                policy_details=body["policy_details"],
-            )
+    #     if existing_policy:
+    #         existing_policy.name = body["name"]
+    #         existing_policy.policy_details = body["policy_details"]
+    #         session.merge(existing_policy)
+    #     else:
+    #         policy = Policy(
+    #             name=body["name"],
+    #             tenant_id=tenant_id,
+    #             policy_details=body["policy_details"],
+    #         )
 
-            session.add(policy)
-            session.commit()
+    #         session.add(policy)
+    #         session.commit()
 
+    # return {
+    #     "statusCode": HTTPStatus.CREATED.value,
+    #     "body": json.dumps({"message": "policy created"}),
+    # }
     return {
         "statusCode": HTTPStatus.CREATED.value,
         "body": json.dumps({"message": "policy created"}),
