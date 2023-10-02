b581c6746453c9df531d7b6581bada5f3793a2ec
gabriel <gabrielfgsantos95@gmail.com>
2023-10-01 22:57:31 -0300

    fix: increase timeout lambda

--- alembic/env.py
+++ alembic/env.py
@@ -2,7 +2,7 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from src.models import models
+import models
 from alembic import context
 
 # this is the Alembic Config object, which provides
